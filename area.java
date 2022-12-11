105b695f1c60ab9a63ea34d737f6c97d50029cca
Akshay <akshaylondhe.2010@gmail.com>
2022-12-11 17:02:55 +0530

    first commit

---
+++ area.java
@@ -0,0 +1,17 @@
+package methods;
+import java.util.*;
+
+public class area {
+
+	
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+double r=sc.nextDouble();
+double area=3.14*r*r;
+System.out.println(area);
+	}}
